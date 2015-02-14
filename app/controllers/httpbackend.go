fc039949c0f554fd7b385b27acc61671936e8a78
Stephen Zhen <eiyaya@gmail.com>
2015-02-14 18:44:24 +0800

    Can push.

--- app/controllers/httpbackend.go
+++ app/controllers/httpbackend.go
@@ -2,6 +2,8 @@ package controllers
 
 import (
 	"fmt"
+	"io"
+	"os"
 	"os/exec"
 
 	"github.com/revel/revel"
@@ -10,6 +12,27 @@ import (
 // HTTPBackEnd (deal with git smart http protcol)
 type HTTPBackEnd struct {
 	*revel.Controller
+}
+
+// GitReceivePack (deal with git push)
+func (h HTTPBackEnd) GitReceivePack() revel.Result {
+	h.Response.Out.Header().Add("Expires", "Fri, 01 Jan 1980 00:00:00 GMT")
+	h.Response.Out.Header().Add("Pragma", "no-cache")
+	h.Response.Out.Header().Add("Cache-Control", "no-cache, max-age=0, must-revalidate")
+	h.Response.Out.Header().Add("Content-Type", "application/x-git-receive-pack-result")
+
+	repo := "/Users/stephenzhen/gitcent-repos/test"
+	git := "/usr/bin/git"
+	cmd := exec.Command(git, "receive-pack", repo, "--stateless-rpc")
+	out, _ := cmd.StdoutPipe()
+	input, _ := cmd.StdinPipe()
+	cmd.Stderr = os.Stderr
+	go io.Copy(h.Response.Out, out)
+	go io.Copy(input, h.Request.Body)
+	cmd.Start()
+	cmd.Wait()
+
+	return h.RenderText("")
 }
 
 // GetInfoRefs (got git repo refs)
